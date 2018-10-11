af8987d6791d472d3507cb9716892f376b8b7439
Cory Kromer-Edwards <14corman@gmail.com>
2018-10-11 14:06:21 -0600

    More work on getting parent container.
    Did more work on the code to be more of what we need.

--- custom_function.py
+++ custom_function.py
@@ -9,29 +9,31 @@ from ..automation.SocketInterface import clientsocket
 class CustomFunctions:
     """Will hold any custom functions we need to collect data."""
 
-	def getParentContainer(**kwargs):
+	def getParentContainer(url, **kwargs):
 		"""Collects the parent container for the url."""
 		driver = kwargs['driver']
 		manager_params = kwargs['manager_params']
-		link_urls = [
+		urls = [
 			x for x in (
 				element.get_attribute("href")
 				for element in driver.find_elements_by_tag_name('a')
 			)
-			if x.startswith(scheme + '://')
+			if x == url
 		]
-		current_url = driver.current_url
-
+		
+		#Need to get the oldest parent and see if it is html or iframe. Will need to be boolean for iframe or not.
+		url_container = zip(urls, url.find_element_by_xpath("..") for url in urls)
+		
 		sock = clientsocket()
 		sock.connect(*manager_params['aggregator_address'])
 
-		query = ("CREATE TABLE IF NOT EXISTS parent_containers ("
-				 "url TEXT, container TEXT);")
+		query = "CREATE TABLE IF NOT EXISTS parent_containers (id NOT NULL INTEGER PRIMARY KEY, url TEXT, container TEXT);"
 		sock.send(("create_table", query))
 
-		query = ('parent_containers', {
-			"url": current_url,
-			"container": link
-		})
-		sock.send(query)
+		for url, container in url_container:
+			query = ('parent_containers', {
+				"url": url,
+				"container": container
+			})
+			sock.send(query)
 		sock.close()
