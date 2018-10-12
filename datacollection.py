4937c19c246edf9de30df667e4f4079d977023a3
Cory Edwards <14corman@gmail.com>
2018-10-12 13:00:53 -0500

    Adding OpenWPM, and changing files
    Added OpenWPM, so that now you can run the code anywhere and it will have OpenWPM there.
    Continued work on datacollection.py so that it now uses a test URL, and is running the test functions.
    Deleted custom_functions.py and put all of its code into datacollection.py so that it can use custom functions properly with OpenWPM.

--- datacollection.py
+++ datacollection.py
@@ -2,18 +2,66 @@ from __future__ import absolute_import
 
 from six.moves import range
 
-from automation import CommandSequence, TaskManager
+from OpenWPM.automation import CommandSequence, TaskManager
 import sys
 import time
+
+from OpenWPM.automation.utilities import db_utils
+from OpenWPM.automation.SocketInterface import clientsocket
+
+class CustomFunctions:
+    """Will hold any custom functions we need to collect data."""
+    def getParentContainer(sites, **kwargs):
+        """Collects the parent container for the url."""
+        driver = kwargs['driver']
+        manager_params = kwargs['manager_params']
+        url_elements = [
+                element.get_attribute("href")
+                for element in driver.find_elements_by_tag_name('a')
+        ]
+    		
+        urls = [
+                url.get_attribute("href") for url in url_elements
+        ]
+    		
+        conatiners = [
+                element.find_element_by_xpath(".//ancestor::iframe") for element in url_elements
+        ]
+    		
+        print("Containers: ", containers)
+    		
+        #Need to get the oldest parent and see if it is html or iframe. Will need to be boolean for iframe or not.
+        url_container = zip(urls, containers)
+    		
+        sock = clientsocket()
+        sock.connect(*manager_params['aggregator_address'])
+    
+        query = "CREATE TABLE IF NOT EXISTS parent_containers (id NOT NULL INTEGER PRIMARY KEY, url TEXT, container TEXT);"
+        sock.send(("create_table", query))
+    
+        for url, container in url_container:
+            query = ('parent_containers', {
+                    "url": url,
+                    "container": container
+                    })
+            sock.send(query)
+        sock.close()
+
+
+
+
 # The list of sites that we wish to crawl
 NUM_BROWSERS = 3
 
 sites = []
-with open('top-1000.csv','rb') as f:
-    for line in f:
-        line = line.replace('\n','')
-	site = line.split(',')[1]
-        sites.append("http://"+site)
+#with open('top-1000.csv','rb') as f:
+#    for line in f:
+#        line = line.replace('\n','')
+#		site = line.split(',')[1]
+#        sites.append("http://"+site)
+
+sites.append("http://Reddit.com")
+
 # Loads the manager preference and 3 copies of the default browser dictionaries
 manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 
@@ -44,9 +92,11 @@ for site in sites:
 
     # Start by visiting the page
     command_sequence.get(sleep=10, timeout=60)
+	
+    command_sequence.run_custom_function(CustomFunctions.getParentContainer, (sites))
 
     # dump_profile_cookies/dump_flash_cookies closes the current tab.
-    command_sequence.dump_profile_cookies(120)
+    #command_sequence.dump_profile_cookies(120)
 
     # index='**' synchronizes visits between the three browsers
     manager.execute_command_sequence(command_sequence, index=None)
