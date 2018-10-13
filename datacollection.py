3d3ed407af0e2182c01112a8a3ae4330359e8ef1
Cory Edwards <14corman@gmail.com>
2018-10-13 13:47:05 -0500

    did more work on datacollection
    Added queues.
    Made the custom function work properly
    Current bug is the while loop is not waiting until the custom function finishes.

--- datacollection.py
+++ datacollection.py
@@ -5,47 +5,63 @@ from six.moves import range
 from OpenWPM.automation import CommandSequence, TaskManager
 import sys
 import time
+import Queue
 
 from OpenWPM.automation.utilities import db_utils
 from OpenWPM.automation.SocketInterface import clientsocket
 
-class CustomFunctions:
-    """Will hold any custom functions we need to collect data."""
-    def getParentContainer(sites, **kwargs):
-        """Collects the parent container for the url."""
-        driver = kwargs['driver']
-        manager_params = kwargs['manager_params']
-        url_elements = [
-                element.get_attribute("href")
-                for element in driver.find_elements_by_tag_name('a')
-        ]
-    		
-        urls = [
-                url.get_attribute("href") for url in url_elements
-        ]
-    		
-        conatiners = [
-                element.find_element_by_xpath(".//ancestor::iframe") for element in url_elements
-        ]
-    		
-        print("Containers: ", containers)
-    		
-        #Need to get the oldest parent and see if it is html or iframe. Will need to be boolean for iframe or not.
-        url_container = zip(urls, containers)
-    		
-        sock = clientsocket()
-        sock.connect(*manager_params['aggregator_address'])
+class ContainerSites:
     
-        query = "CREATE TABLE IF NOT EXISTS parent_containers (id NOT NULL INTEGER PRIMARY KEY, url TEXT, container TEXT);"
-        sock.send(("create_table", query))
+    def __init__(self, sites):
+        #maxsize <= 0 means infinite size queue.
+        #OpenWPM uses Python 2.7, so need Q instead of q.
+        self.queue = Queue.Queue(maxsize = -1)
+        
+        for site in sites:
+            self.queue.put(site)
+            
+        self.inside = True
     
-        for url, container in url_container:
-            query = ('parent_containers', {
-                    "url": url,
-                    "container": container
-                    })
-            sock.send(query)
-        sock.close()
+def getParentContainer(container_class, **kwargs):
+    """A custom function that detects if a tags on the page are 
+    inside of an iframe or not."""
+    container_class.inside = True
+    driver = kwargs['driver']
+    manager_params = kwargs['manager_params']
+    url_elements = [
+            element for element in driver.find_elements_by_tag_name('a')
+    ]
+		
+    urls = []
+    containers = []
+    for element in url_elements:
+        urls.append(element.get_attribute("href"))
+        try:
+            element.find_element_by_xpath(".//ancestor::iframe") 
+            containers.append(True)
+        except:
+            containers.append(False)
+		
+    print("Containers: ", containers)
+		
+    #Need to get the oldest parent and see if it is html or iframe. Will need to be boolean for iframe or not.
+    url_container = zip(urls, containers)
+		
+    sock = clientsocket()
+    sock.connect(*manager_params['aggregator_address'])
+
+    query = "CREATE TABLE IF NOT EXISTS parent_containers (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, url TEXT, iframe_container INTEGER);"
+    sock.send(("create_table", query))
+
+    for url, container in url_container:
+        query = ('parent_containers', {
+                "url": url,
+                "iframe_container": container
+                })
+        sock.send(query)
+        container_class.queue.put(url)
+    sock.close()
+    container_class.inside = False
 
 
 
@@ -59,10 +75,12 @@ sites = []
 num_websites = 1
 with open('top-1m.csv','rb') as f:
     for x in xrange(num_websites):
-        line = next(myfile)
-        line = line.replace('\n','')
-	     site = line.split(',')[1]
-        sites.append("http://"+site)
+        line = next(f)
+        line = line.replace('\r\n','')
+        site = line.split(',')[1]
+        sites.append("http://" + site)
+        
+container = ContainerSites(sites)
 
 # Loads the manager preference and 3 copies of the default browser dictionaries
 manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
@@ -89,16 +107,15 @@ manager_params['database_name'] = 'output_data.sqlite'
 manager = TaskManager.TaskManager(manager_params, browser_params)
 
 # Visits the sites with all browsers simultaneously
-for site in sites:
-    command_sequence = CommandSequence.CommandSequence(site)
+#This while loop is still breaking out too early. It will not process more
+#than 1 site at a time if 1 is in the queue.
+while container.inside and not container.queue.empty():
+    command_sequence = CommandSequence.CommandSequence(container.queue.get())
 
     # Start by visiting the page
     command_sequence.get(sleep=10, timeout=60)
 	
-    command_sequence.run_custom_function(CustomFunctions.getParentContainer, (sites))
-
-    # dump_profile_cookies/dump_flash_cookies closes the current tab.
-    #command_sequence.dump_profile_cookies(120)
+    command_sequence.run_custom_function(getParentContainer, (container,))
 
     # index='**' synchronizes visits between the three browsers
     manager.execute_command_sequence(command_sequence, index=None)
